cfa371fa4488f73b5d07e72bf3950e5dd36d1f9a
Youssef Daood <ydaood@arkdev.net>
2020-11-02 22:31:32 +0200

    Simplify if condition.

--- src/App.js
+++ src/App.js
@@ -29,9 +29,11 @@ import Footer from './Components/Home/Footer/Footer';
 import {translate , getLanguage} from 'react-switch-lang';
 
 function App() {
+  const direction = ['he','ar'].includes(getLanguage()) ? 'rtl' : 'ltr';
+
   return (
     <Router onUpdate={() => window.scrollTo(0, 0)}>
-      <div className="App" dir={getLanguage()==='he' || getLanguage()==='ar'?'rtl':'ltr'}>
+      <div className="App" dir={direction}>
           <HeaderTop />
           <Header/>
           <React.StrictMode>
