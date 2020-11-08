0649e634bcef65938f3d31ea8d6b79f9d0a45bd3
mariam samy makram <mariam.samy.mrmr@gmail.com>
2020-11-08 13:06:51 +0200

    fix in error404.

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -13,7 +13,7 @@ class Error extends Component{
         return(
             <section className="error pt-5 pb-5 text-center mt-5">
                 <Container className="pt-5 pb-5 mt-md-5">
-                    <h1 className={getLanguage()==='ar'?'GE_SS':'times'}>404</h1>
+                    <h1 className={getLanguage()==='ar'?'times':'times'}>404</h1>
                     <p className={getLanguage()==='ar'?'GE_SS':'lota'}>{t('error.message')}</p>
                 </Container>
             </section>
